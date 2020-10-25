50456b66551fb1f55c6e8ec393b167bfc64dbbb0
Helga Maznikava <helga@helgas-air.lan>
2020-10-25 10:30:58 -0700

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const tmp = [];
+  const openBrackets = [];
+  const closeBrackets = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      openBrackets.push(bracketsConfig[i][0]);
+      closeBrackets.push(bracketsConfig[i][1]);
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (closeBrackets.includes(str[i])) {
+      const oppositeOpenedBracket = openBrackets[closeBrackets.indexOf(str[i])];
+      if (tmp[tmp.length - 1] === oppositeOpenedBracket) {
+        tmp.pop();
+      } else if (str[i] !== oppositeOpenedBracket) {
+        return false;
+      } else {
+        tmp.push(str[i]);
+      }
+    } else {
+      tmp.push(str[i]);
+    }
+  }
+  return tmp.length === 0;
 }
